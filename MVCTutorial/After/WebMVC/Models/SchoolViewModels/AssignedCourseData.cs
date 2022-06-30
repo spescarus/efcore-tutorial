e376f31144499da9bf5b85e77489e1743e729d75
Sergiu Ioan Pescarus <sergiu.pescarus@siemens.com>
2022-06-30 16:50:49 +0300

    Remove unused code

--- MVCTutorial/After/WebMVC/Models/SchoolViewModels/AssignedCourseData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebMvc.Models.SchoolViewModels;
-
-public class AssignedCourseData
-{
-    public int    CourseId { get; set; }
-    public string Title    { get; set; }
-    public bool   Assigned { get; set; }
-}
